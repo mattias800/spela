484c84f33736bab72899dd2588da863fecd6f627
Mattias Andersson <mattias800@gmail.com>
2026-04-12 12:08:42 +0200

    feat: add centralized carousel card constants

---
+++ web/src/lib/carousel-constants.ts
@@ -0,0 +1,5 @@
+/** Fixed height (px) for game cards in carousel shelves. */
+export const CAROUSEL_CARD_HEIGHT = 240;
+
+/** Default aspect ratio (width/height) for placeholder/skeleton cards. */
+export const CAROUSEL_DEFAULT_ASPECT_RATIO = 3 / 4;
